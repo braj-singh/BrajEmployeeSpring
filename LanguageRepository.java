5019ae7e9861a2ae01ee64a8d5d79eeebeb39671
braj-singh <101088550+braj-singh@users.noreply.github.com>
2022-03-15 17:22:36 +0530

    Add files via upload

---
+++ LanguageRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LanguageRepository extends CrudRepository<Language, String>{
+	public List<Language>  findByEmployeeId(String employeeId);
+
+}
